eeb7af6ede7c530c249fc68dbede5670e3254a37
jehincastic <jehincastic@gmail.com>
2022-03-07 03:39:29 +0530

    Added Title

--- components/SocialIcons.js
+++ components/SocialIcons.js
@@ -2,10 +2,12 @@ import { FaGithub, FaLinkedinIn } from "react-icons/fa";
 
 const links = [
   {
+    title: "Github",
     Icon: FaGithub,
     href: "https://github.com/jehincastic/",
   },
   {
+    title: "LinkedIn",
     Icon: FaLinkedinIn,
     href: "https://www.linkedin.com/in/jehincastic/",
   },
@@ -13,7 +15,7 @@ const links = [
 
 const SocialIcons = () => (
   <div className="flex flex-row text-2xl my-6 text-gray-500 dark:text-gray-300">
-    {links.map(({ Icon, href }, i) => (
+    {links.map(({ Icon, href, title }, i) => (
       <a
         key={href}
         href={href}
@@ -23,7 +25,7 @@ const SocialIcons = () => (
           i < links.length - 1 ? "mr-3" : ""
         }`}
       >
-        <Icon />
+        <Icon title={title} />
       </a>
     ))}
   </div>
