fef73fb916492ce38be5d23fbf38a0653c5a97b2
jehincastic <jehincastic@gmail.com>
2022-03-07 21:36:22 +0530

    Add 404

--- components/PageTitle.js
+++ components/PageTitle.js
@@ -8,11 +8,11 @@ const PageTitle = ({
   marginTop = "mt-14 lg:mt-32",
 }) => (
   <div className={`${marginTop} font-light w-full text-black dark:text-white`}>
-    <h1 className={`text-4xl sm:text-5xl lg:text-6xl mb-2 ${center && "md:text-center"}`}>
+    <h1 className={`text-4xl sm:text-5xl lg:text-6xl mb-2 ${center && "text-center"}`}>
       {title}
     </h1>
     {subtitle && (
-      <p className="text-2xl sm:text-3xl lg:text-4xl w-11/12 sm:w-5/6 md:w-11/12 lg:w-4/5 xl:w-3/5">
+      <p className={`text-2xl ${center && "text-center mx-auto"} sm:text-3xl lg:text-4xl w-11/12 sm:w-5/6 md:w-11/12 lg:w-4/5 xl:w-3/5`}>
         {subtitle}
       </p>
     )}
---
+++ pages/404.js
@@ -0,0 +1,59 @@
+import React from "react";
+import { AnimatePresence, motion } from "framer-motion";
+
+import Layout from "../components/Layout";
+import Link from "../components/Link";
+import PageTitle from "../components/PageTitle";
+
+const title = "Not Found";
+const subtitle = "The page you are looking for doesn't exist.";
+
+const NotFound = () => {
+  return (
+    <AnimatePresence>
+      <Layout
+        title="Home"
+        description={`${title} - ${subtitle}`}
+        divClass="bg-gray-100 dark:bg-gray-900 pb-12 mb-10"
+      >
+        <PageTitle
+          title="404"
+          subtitle={subtitle}
+          center
+          icons={false}
+          marginTop="mt-12 lg:mt-12 font-bold"
+        />
+        <Link href={"/"}>
+          <a className="text-balck dark:text-white mt-5 text-center font-medium text-xl mx-auto">
+            Back Home
+            <motion.div
+              layoutId="navigation-underline"
+              className="navigation-underline"
+              initial={{
+                borderWidth: "0px",
+                marginLeft: "0px",
+                marginRight: "0px",
+              }}
+              animate={{
+                borderWidth: "1px",
+                marginRight: ["2px", "0px", "2px", "0px"],
+                marginLeft: ["0px", "2px", "0px", "2px", "0px"],
+              }}
+              transition={{
+                duration: 0.5,
+                ease: "easeInOut",
+              }}
+              exit={{
+                borderWidth: "0px",
+                marginLeft: "0px",
+                marginRight: "0px",
+              }}
+            />
+          </a>
+        </Link>
+      </Layout>
+    </AnimatePresence>
+  );
+};
+
+export default NotFound;
