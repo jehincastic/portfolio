5d8e142912f5e79182789e761af3bb79c443a110
jehincastic <jehincastic@gmail.com>
2022-03-13 01:32:32 +0530

    Added Eslint Rule

--- components/Card.js
+++ components/Card.js
@@ -1,7 +1,5 @@
 import React from "react";
 
-import Link from "./Link";
-
 const Card = ({
   title,
   description,
--- components/Section.js
+++ components/Section.js
@@ -1,11 +1,7 @@
-import { ReactNode } from "react";
-
-const linebreakStyles = "mt-24 md:mt-32 xl:mt-44";
-
 const Section = ({ children, linebreak = false }) => (
   <section
     className={`text-base sm:text-lg font-light leading-relaxed lg:w-4/5 2xl:w-2/3 text-gray-600 dark:text-gray-300 ${
-      linebreak ? linebreakStyles : "mt-12"
+      linebreak ? "mt-24 md:mt-32 xl:mt-44" : "mt-12"
     }`}
   >
     {children}
