3addfdb9c5a51ed5a8231b9f968d71be320c6717
jehincastic <jehincastic@gmail.com>
2022-03-13 00:36:22 +0530

    Update Title

--- config/pages/home.js
+++ config/pages/home.js
@@ -1,7 +1,7 @@
 const config = {
   pageName: "Home",
   title: "Hello, I'm Castic 👋",
-  subtitle: "I'm a software developer at Crayon'd, and living in Nagercoil, India.",
+  subtitle: "I'm a software developer at Crayon'd and currently in Nagercoil, India.",
   subHeading: "About",
   subContent: `I like to spend most of my time as a backend developer.
   My go to tool for backend developement is Node JS.
