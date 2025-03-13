b67a6b3ca43c556c273b3c62d9112e1bcd497094
Castic Jehin <jehincastic@gmail.com>
2025-03-13 11:31:14 +0530

    Update home.js

--- config/pages/home.js
+++ config/pages/home.js
@@ -3,11 +3,11 @@ const config = {
   title: "Hello, I'm Castic 👋",
   subtitle: "I'm a Software Engineer at SenseHawk and currently in Nagercoil, India.",
   subHeading: "About",
-  subContent: `I like to spend most of my time as a backend developer.
-  My go to tool for backend developement is Node JS.
-  Sometimes I also work using React.js.
-  I'm currently experimenting with Docker and Kubernetes.
-  I'm also plannig to learn about serverless technologies on AWS.`,
+  subContent: `As a dedicated backend developer, I specialize in creating efficient, scalable, and high-performance applications.
+  My primary expertise lies in Node.js, which I leverage to build robust and reliable backend systems.
+  Additionally, I’m skilled in React.js, allowing me to craft dynamic front-end experiences when the project requires it.
+  Currently, I’m deepening my understanding of containerization with Docker and Kubernetes to optimize development workflows and improve application scalability.
+  I’m also exploring serverless architectures, particularly within AWS, to create cloud-native solutions and stay at the forefront of modern development practices.`,
 };
 
 export default config;
